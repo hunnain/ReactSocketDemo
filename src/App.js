3c0c507deb1ea968aeae497e4ca31d1d3448fda1
Hunnain Pasha <hunnainpashahgchgc@gmail.com>
2019-04-04 02:33:44 +0500

    App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from "socket.io-client";
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      response: false,
+      endpoint: "http://127.0.0.1:5000"
+    };
+  }
+  componentWillMount(){
+    console.log("First Run")
+    axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+    axios.post('http://127.0.0.1:5000/addmessage',{
+      senderId:"5c6c2e94c156721f09f5fe8e",
+      recieverId:"5c6c3264c156721f09f5fe92",
+      message:"Hello Henry"
+    }, 
+    ).then((res)=>{
+      console.log("Response",res)
+    })
+  }
+  componentDidMount() {
+    console.log("Run")
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("message", data => console.log("Data",data));
+  }
   render() {
     return (
       <div className="App">
